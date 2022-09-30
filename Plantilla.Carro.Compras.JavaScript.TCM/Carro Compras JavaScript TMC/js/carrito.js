9851a622bd3baa730ecd7fa7db2d9ba46540dc28
HerlanQuispe <113206194+HerlanQuispe@users.noreply.github.com>
2022-09-30 19:01:06 -0400

    Add files via upload

---
+++ Plantilla.Carro.Compras.JavaScript.TCM/Carro Compras JavaScript TMC/js/carrito.js
@@ -0,0 +1,28 @@
+﻿class Carrito{
+
+    //Añadir el producto al carrito
+    comprarProducto(e){
+        e.preventDefault();
+        if(e.target.classList.contains("agregar carrito")){
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatosProducto(producto);
+        }
+    }
+
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen : producto.querySelector("img").src,
+            titulo : producto.querySelector("h4").textCont,
+            precio : producto.querySelector(".precio span").textCont,
+            id : producto.querySelector("a").getAttribute("data-id"),
+            cantidad : 1
+        }
+        this.insertarcarrito(infoProducto);
+
+    }
+    
+    insertarcarrito(producto){
+        const rom = document.createElement("tr");
+        row.innerHTML = "<td><img src=""</td>"
+    }
+}
